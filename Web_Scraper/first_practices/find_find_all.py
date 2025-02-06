9fb6d98181de7a63a8d7912b6b99fc7ad1ebf48f
Noons-ledev <noah.briet@icloud.com>
2025-02-06 11:03:09 +0100

    Here we are splitting all <a> from the html page we got with BS, and retrieving class and links of those balises

--- Web_Scraper/first_practices/find_find_all.py
+++ Web_Scraper/first_practices/find_find_all.py
@@ -6,18 +6,16 @@ import requests
 url = 'https://www.scrapethissite.com/pages/simple/'
 request = requests.get(url)
 
-# Print the response headers to check for redirects or content type issues
-print(f"Response Headers: {request.headers}")
-
 # Check if the request is successful
 if request.status_code == 200:
-    # Print the raw content of the page to see if it contains HTML
-    print("Page Content:", request.text[:500])  # Only print the first 500 characters to avoid flooding the terminal
-    
     # Pass the page content to BeautifulSoup
     soup = BeautifulSoup(request.text, 'html.parser')
+    soup_links = soup.find_all('a')
+    for links in soup_links:
+        print("Class : ", links.get('class'))
+        print("Link : ", links.get('href'))
     
-    # Print out the parsed HTML (to see if BeautifulSoup is working)
-    print(soup.prettify())  # prettify() helps format the output for readability
 else:
     print(f"Failed to retrieve the page. Status code: {request.status_code}")
+
+
